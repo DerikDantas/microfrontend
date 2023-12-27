90e0d348654890c45dd30465a6dbf6ab29ab86b2
Derik Dantas <derikdantas@hotmail.com>
2023-12-27 15:40:06 -0300

    fix: fixing env vite configs

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,36 +1,28 @@
 import federation from "@originjs/vite-plugin-federation";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-const isProd = import.meta.env?.VITE_NODE_ENV === "production";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "host",
-      remotes: {
-        orderApp:
-          `${
-            isProd
-              ? "https://microfrontend-order-app.vercel.app"
-              : "https://localhost:5001"
-          }` + "/assets/orderApp.js",
-        productApp:
-          `${
-            isProd
-              ? "https://microfrontend-product-app.vercel.app"
-              : "https://localhost:5002"
-          }` + "/assets/productApp.js",
-      },
-      shared: ["react", "react-dom", "react-router-dom"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "host",
+        remotes: {
+          orderApp: `${env.VITE_HOST_ORDER_APP}` + "assets/orderApp.js",
+          productApp: `${env.VITE_HOST_PRODUCT_APP}` + "assets/productApp.js",
+        },
+        shared: ["react", "react-dom", "react-router-dom"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
